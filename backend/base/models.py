369d875626801a3ebbc9788a5a72f1be7d52860f
Eduard Ruiz Munné <eduardrm2000@gmail.com>
2023-11-06 17:31:55 +0100

    Solved problem backend

---
+++ backend/base/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Recipe(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    text = models.TextField()
