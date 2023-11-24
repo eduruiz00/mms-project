43c347969d4c97981b1751de97c04f29bfd09251
Eduard Ruiz Munné <eduardrm2000@gmail.com>
2023-11-24 15:13:39 +0100

    Front-end image after recipe

--- frontend/src/views/ReciepeView.vue
+++ frontend/src/views/ReciepeView.vue
@@ -1,20 +1,41 @@
 <template>
   <div>
     <h1>Generated recipe</h1>
-    <h1>{{ generatedRecipe.recipe.Title }}</h1>
-    <img :src="'data:image/jpeg;base64,' + generatedRecipe.Image" alt="Image">
-    <p>{{ generatedRecipe.recipe.QuantitiesRequired }}</p>
-    <p>{{ generatedRecipe.recipe.Instructions }}</p>
+    <h1 v-if="generatedRecipe">{{ generatedRecipe.recipe.Title }}</h1>
+    <div class="w-full flex justify-center">
+      <img v-if="generatedImage" :src="'data:image/jpeg;base64,' + generatedImage" alt="Image">
+      <div class="h-56 w-96 bg-gray-200" v-else></div>
+    </div>
+    <p v-if="generatedRecipe">{{ generatedRecipe.recipe.QuantitiesRequired }}</p>
+    <p v-if="generatedRecipe">{{ generatedRecipe.recipe.Instructions }}</p>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: 'ReciepeView',
   components: {},
+  created() {
+    if (this.generatedRecipe) {
+      const request = {
+      Title: this.generatedRecipe.recipe.Title,
+      Description: this.generatedRecipe.recipe.Description,
+    }
+    axios.post('http://127.0.0.1:8000/recipes/image/', request)
+          .then(response => {
+            console.log(response)
+            this.generatedImage = response.data.Image;
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    }
+  },
   data() {
     return {
-
+      generatedImage: null,
     }
   },
   methods: {
